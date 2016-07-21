41a002c0674f83f386797228fa509e92a7cb5f30
Marijn van Vliet <w.m.vanvliet@gmail.com>
2016-07-21 11:02:34 +0300

    missing file

---
+++ KerbulatorOptions.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Kerbulator {
+	public class KerbulatorOptions {
+		public Rect mainWindowPos = new Rect(0, 60, 280, 400);
+		public Rect runWindowPos = new Rect(0, 470, 200, 200);
+		public Rect editWindowPos = new Rect(280, 60, 500, 400);
+		public Rect repeatWindowPos = new Rect(200, 470, 200, 100);
+	}
+}
