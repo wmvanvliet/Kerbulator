ded2fbbbb2f2cea4f14d0fbbc2cbb8eb92cf1a9a
Marijn van Vliet <w.m.vanvliet@gmail.com>
2019-07-01 13:59:44 +0300

    Fix compilation on windows

--- compile.bat
+++ compile.bat
@@ -1,15 +1,19 @@
+echo off
+
 REM Location of your KSP installation
 SET KSPDIR=C:\Program Files (x86)\Steam\SteamApps\common\Kerbal Space Program
 
 REM Location of the Unity assembly files inside your KSP installation
-SET MANAGED=KSP_Data\Managed
+SET MANAGED=KSP_x64_Data\Managed
 
 REM This should be mono version 3.x (newer versions of mono dropped .NET 3.5 support)
 SET MCS=C:\Program Files (x86)\Mono\bin\mcs
 
 REM Compile the plugin
 mkdir build
-"%MCS%" -t:library -lib:"%KSPDIR%\%MANAGED%" -r:Assembly-CSharp,Assembly-CSharp-firstpass,UnityEngine,UnityEngine.CoreModule,UnityEngine.IMGUIModule,UnityEngine.AnimationModule -out:build\Kerbulator.dll *.cs
+echo on
+"%MCS%" -t:library -lib:"%KSPDIR%\%MANAGED%" -r:Assembly-CSharp,Assembly-CSharp-firstpass,UnityEngine -out:build\Kerbulator.dll *.cs
+echo off
 
 REM Collect the plugin files in a folder called "package\Kerbulator"
 mkdir package\Kerbulator\Plugins
